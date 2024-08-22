d17bdaa6e86b2d13eb13cf1d169a4c8fe675431a
DavRip77 <167356811+DavRip77@users.noreply.github.com>
2024-08-22 14:06:08 -0500

    Update admin.py
    Actualización con las clases e importaciones

--- Anastyles/mi_app/admin.py
+++ Anastyles/mi_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    list_display_links = ('name',)
+    list_editable = ('price', 'stock')
+    search_fields = ('name',)
